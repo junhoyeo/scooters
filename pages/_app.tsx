b208e6fd3a07aa5a031bce16328bce6818369048
Junho Yeo <hanaro0704@gmail.com>
2021-05-23 04:29:31 +0900

    카카오 지도 API 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,10 @@ function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="shortcut icon" href="icons/favicon.ico" />
+        <script
+          type="text/javascript"
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=27b23dc85a0dcc560fd27104acd30069"
+        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
